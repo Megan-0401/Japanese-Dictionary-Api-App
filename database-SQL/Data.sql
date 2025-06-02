6c26de1b9000a5a909f4a903d2b224aad38a22b7
Megan O <meganoconnor2005@gmail.com>
2025-06-02 21:50:38 +0100

    adding database tests

---
+++ database-SQL/Data.sql
@@ -0,0 +1,40 @@
+USE dictionary;
+
+-- adding word classes
+
+INSERT INTO word_classes (class)
+VALUES ('pronoun'), ('noun'), ('-i adjective'), ('-na adjective'), ('verb'), ('determiner'), ('preposition');
+
+-- adding word categories
+
+INSERT INTO categories (category)
+VALUES ('animals'), ('buildings'), ('countries & languages'), ('clothes & fashion'), ('creativity'), ('date & time'), ('directions'), 
+('emotions'), ('family'), ('food'), ('literature'), ('negative'), ('occupations'), ('people'), ('positive'), ('relationships'), ('school'),
+('shopping'), ('size'), ('sports'), ('traits'), ('transport'), ('weather'), ('work'), ('unclassified');
+
+INSERT INTO kanji (kanji)
+VALUES ('学生'), ('医者'), ('車'), ('高い'), ('鉛筆'), ('安い'), ('寿司'), ('今日');
+
+INSERT INTO hiragana (hiragana)
+VALUES ('がくせい'), ('あなた'), ('いしゃ'), ('くるま'), ('たかい'), ('えんぴつ'), ('やすい'), ('すし'), ('おいしい'), ('きょう');
+
+INSERT INTO romanji (romanji)
+VALUES ('gakusei'), ('anata'), ('isha'), ('kuruma'), ('takai'), ('enpitsu'), ('yasui'), ('sushi'), ('oishii'), ('kyou');
+
+INSERT INTO meanings (english)
+VALUES ('student'), ('you'), ('doctor'), ('car'), ('tall'), ('expensive'), ('pencil'), ('cheap'), ('sushi'), ('delicious/tasty'), ('today');
+
+INSERT INTO sentences (jp_sentence, eng_sentence)
+Values ('あなたは学生ですか？', 'Are you a student?'), ('僕の母は医者です。', 'My mum/mom is a doctor.'),
+('その車はとても高いです！', 'That car is very expensive!'), ('鉛筆はどこですか？', 'Where is the pencil?'),
+('この店でりんごが安いですね。', "The apples at this store are cheap, aren't they?"), ('おいしい寿司が好きです。', 'I like delicious sushi.'),
+('今日は私の誕生日です。', 'Today is my birthday.');
+
+INSERT INTO words (kanji_id, hiragana_id, romanji_id, class_id, sentence_id)
+VALUES (1, 1, 1, 2, 1),(2, 3, 3, 2, 2);
+
+INSERT INTO words (hiragana_id, romanji_id, class_id, sentence_id)
+VALUES (2, 2, 1, 1);
+
+INSERT INTO word_meanings
+VALUES (1, 1), (2, 2), (3, 3);
---
+++ database-SQL/Table Outline.sql
@@ -0,0 +1,88 @@
+CREATE DATABASE dictionary;
+
+USE dictionary;
+
+CREATE TABLE kanji (
+id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+kanji VARCHAR (20) NOT NULL,
+PRIMARY KEY (id));
+
+CREATE TABLE hiragana (
+id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+hiragana VARCHAR (50) NOT NULL,
+PRIMARY KEY (id));
+
+CREATE TABLE katakana (
+id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+katakana VARCHAR (50) NOT NULL,
+PRIMARY KEY (id));
+
+CREATE TABLE romanji (
+id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+romanji VARCHAR (50) NOT NULL,
+PRIMARY KEY (id));
+
+CREATE TABLE word_classes (
+id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+class VARCHAR (50) NOT NULL,
+PRIMARY KEY (id));
+
+CREATE TABLE sentences (
+id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+jp_sentence VARCHAR (100) NOT NULL,
+eng_sentence VARCHAR (100) NOT NULL,
+PRIMARY KEY (id));
+
+CREATE TABLE words (
+id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+kanji_id INT UNSIGNED,
+hiragana_id INT UNSIGNED,
+katakana_id INT UNSIGNED,
+romanji_id INT UNSIGNED NOT NULL,
+class_id INT UNSIGNED NOT NULL,
+sentence_id INT UNSIGNED NOT NULL,
+PRIMARY KEY (id),
+FOREIGN KEY (kanji_id) REFERENCES kanji(id),
+FOREIGN KEY (hiragana_id) REFERENCES hiragana(id),
+FOREIGN KEY (katakana_id) REFERENCES katakana(id),
+FOREIGN KEY (romanji_id) REFERENCES romanji(id),
+FOREIGN KEY (class_id) REFERENCES word_classes(id),
+FOREIGN KEY (sentence_id) REFERENCES sentences(id)
+);
+
+CREATE TABLE meanings (
+id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+english VARCHAR (50),
+PRIMARY KEY (id));
+
+CREATE TABLE word_meanings (
+word_id INT UNSIGNED NOT NULL,
+meaning_id INT UNSIGNED NOT NULL,
+PRIMARY KEY (word_id, meaning_id),
+FOREIGN KEY (word_id) REFERENCES words(id),
+FOREIGN KEY (meaning_id) REFERENCES meanings(id));
+
+CREATE TABLE categories (
+id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+category VARCHAR (50),
+PRIMARY KEY (id));
+
+CREATE TABLE word_categories (
+word_id INT UNSIGNED NOT NULL,
+category_id INT UNSIGNED NOT NULL,
+PRIMARY KEY (word_id, category_id),
+FOREIGN KEY (word_id) REFERENCES words(id),
+FOREIGN KEY (category_id) REFERENCES categories(id));
+
+CREATE TABLE users (
+id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+username VARCHAR (50),
+password VARCHAR (50),
+PRIMARY KEY (id));
+
+CREATE TABLE bookmarks (
+word_id INT UNSIGNED NOT NULL,
+user_id INT UNSIGNED NOT NULL,
+PRIMARY KEY (word_id, user_id),
+FOREIGN KEY (word_id) REFERENCES words(id),
+FOREIGN KEY (user_id) REFERENCES users(id));
