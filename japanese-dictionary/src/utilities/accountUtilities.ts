1e6a1d66657be5397054483aee7add367f099139
Megan O <meganoconnor2005@gmail.com>
2025-06-12 14:10:43 +0100

    adding account form switch functionality

--- japanese-dictionary/src/utilities/accountUtilities.ts
+++ japanese-dictionary/src/utilities/accountUtilities.ts
@@ -1,3 +1,5 @@
+import "../style.scss";
+
 //CAPTURING DOM ELEMENTS//
 
 //FORM BUTTONS//
@@ -5,20 +7,37 @@ const loginFormBtn = document.querySelector<HTMLButtonElement>("#loginFormBtn");
 const signupFormBtn = document.querySelector<HTMLButtonElement>("#signupFormBtn");
 
 //FORM INPUTS//
-const usernameInput = document.querySelector<HTMLInputElement>("#username");
-const passwordInput = document.querySelector<HTMLInputElement>("#password");
+const usernameInput = document.querySelector<HTMLInputElement>("#usernameInput");
+const passwordInput = document.querySelector<HTMLInputElement>("#passwordInput");
 
 //SUBMIT BUTTONS//
-const submitLoginBtn = document.querySelector<HTMLButtonElement>("submitLogin");
-const submitSignupBtn = document.querySelector<HTMLButtonElement>("submitSignup");
+const loginBtn = document.querySelector<HTMLButtonElement>("#loginBtn");
+const signupBtn = document.querySelector<HTMLButtonElement>("#signupBtn");
 
 if (
 	!loginFormBtn ||
 	!signupFormBtn ||
 	!usernameInput ||
 	!passwordInput ||
-	!submitLoginBtn ||
-	!submitSignupBtn
+	!loginBtn ||
+	!signupBtn
 ) {
 	throw new Error("Some elements could not be found.");
 }
+
+//METHODS//
+
+//EVENT HANDLERS//
+const handleLoginFormBtnOnClick = () => {
+	loginBtn.style.display = "initial";
+	signupBtn.style.display = "none";
+};
+
+const handleSingupFormBtnOnClick = () => {
+	loginBtn.style.display = "none";
+	signupBtn.style.display = "initial";
+};
+
+//EVENT LISTENERS//
+loginFormBtn.addEventListener("click", handleLoginFormBtnOnClick);
+signupFormBtn.addEventListener("click", handleSingupFormBtnOnClick);
